bdd44b2b2e23b917fd0eba687ae29a5b44783195
JosephMuriithiWarui <waruijoseph15@gmail.com>
2025-11-07 08:56:09 +0300

    server uploaded

--- server/server.js
+++ server/server.js
@@ -7,12 +7,17 @@ const productRoutes = require('./routes/productRoutes');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+connectDB();
+
 app.use(express.json());
 app.use('/api/farmers', farmerRoutes);
 app.use('/api/buyers', buyerRoutes);
 app.use('/api/products', productRoutes);
 
+app.get('/', (req, res) => {
+    res.send('🌿 Welcome to SokoSmart API!');
+});
+
 app.listen(PORT, () => {
     console.log(`🚀 Server is running on port ${PORT}`);
-    connectDB();
 });
